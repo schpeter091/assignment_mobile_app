acb0446a6b5285a3b812578469f9371d0c38a933
schpeter091 <peter.schwarcz@gmail.com>
2021-11-12 18:58:55 +0000

    add restaurant function for admins

--- app/src/main/java/com/example/assignment/AddRestaurantActivity.java
+++ app/src/main/java/com/example/assignment/AddRestaurantActivity.java
@@ -1,14 +1,87 @@
 package com.example.assignment;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
 
 public class AddRestaurantActivity extends AppCompatActivity {
+
+    Button button;
+    EditText streetName;
+    EditText restaurantName;
+    EditText postCode;
+    ImageView picRestaurant;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_restaurant);
+
+        button = findViewById(R.id.button_addRestaurant);
+        streetName = findViewById(R.id.etSteetName);
+        restaurantName = findViewById(R.id.etRestaurantName);
+        postCode = findViewById(R.id.etPostCode);
+        picRestaurant = findViewById(R.id.picRestaurant);
+
+        picRestaurant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
+    }
+
+
+
+
+    private void selectImage(){
+        ImagePicker.with(this)
+                .crop()	    			//Crop image(Optional), Check Customization for more option
+                .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                .start();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            //Image Uri will not be null for RESULT_OK
+            Uri uri = data.getData();
+
+            // Use Uri object instead of File to avoid storage permissions
+            picRestaurant.setImageURI(uri);
+        }
+    }
+
+
+    private boolean validateData(){
+if(restaurantName.getText().toString().isEmpty()){
+    Toast.makeText(AddRestaurantActivity.this,"Please enter a restaurant name! ",Toast.LENGTH_LONG).show();
+    return false;
+}
+if(streetName.getText().toString().isEmpty()){
+    Toast.makeText(AddRestaurantActivity.this, "Please enter a street name! ", Toast.LENGTH_LONG).show();
+    return false;
+
+}
+if (postCode.getText().toString().isEmpty()){
+    Toast.makeText(AddRestaurantActivity.this, "Enter a postcode! ", Toast.LENGTH_LONG).show();
+
+}
     }
 }
---
+++ app/src/main/java/com/example/assignment/model/RestaurantData.java
@@ -0,0 +1,50 @@
+package com.example.assignment.model;
+
+public class RestaurantData {
+
+    private String uid;
+    private String imageUrl;
+    private String name;
+    private String postcode;
+    private String streetName;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+}

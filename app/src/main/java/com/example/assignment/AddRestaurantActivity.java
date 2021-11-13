dd3e08d9cca71b90605a0e5d72c9754ac4cac050
schpeter091 <peter.schwarcz@gmail.com>
2021-11-13 10:07:13 +0000

    saving data and jpg to firebase creating new item for restaurants get the list item(restaurants from firebase)

--- app/src/main/java/com/example/assignment/AddRestaurantActivity.java
+++ app/src/main/java/com/example/assignment/AddRestaurantActivity.java
@@ -1,5 +1,6 @@
 package com.example.assignment;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,7 +14,18 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.assignment.model.RestaurantData;
+import com.example.assignment.model.UserData;
 import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.Date;
 
 public class AddRestaurantActivity extends AppCompatActivity {
 
@@ -22,6 +34,7 @@ public class AddRestaurantActivity extends AppCompatActivity {
     EditText restaurantName;
     EditText postCode;
     ImageView picRestaurant;
+    private Uri uri;
 
 
     @Override
@@ -42,16 +55,67 @@ public class AddRestaurantActivity extends AppCompatActivity {
             }
         });
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+            }
+        });
+
     }
 
 
+    private void saveData(){
+        if(validateData()){
+            FirebaseStorage storage =  FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference();
+
+            // Create a reference to "mountains.jpg"
+            StorageReference restaurantsRef = storageRef.child("restarant_"+restaurantName.getText()+"-"+new Date() +".jpg");
+
+            restaurantsRef.putFile(uri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if(task.isSuccessful()){
+                        restaurantsRef.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Uri> task) {
+                                String url = task.getResult().toString();
+
+                                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                                RestaurantData restaurantData =  new RestaurantData();
+                                restaurantData.setName(restaurantName.getText().toString());
+                                restaurantData.setImageUrl(url);
+                                restaurantData.setPostcode(postCode.getText().toString());
+                                restaurantData.setStreetName(streetName.getText().toString());
+                                restaurantData.setUid(FirebaseAuth.getInstance().getCurrentUser().getUid());
 
 
-    private void selectImage(){
+                                db.collection("restaurant").document().set(restaurantData)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if(task.isSuccessful()){
+                                                    Toast.makeText(AddRestaurantActivity.this, " Upload successful", Toast.LENGTH_LONG).show();
+
+                                                }
+                                            }
+                                        });
+                            }
+                        });
+                    }
+                }
+            });
+
+        }
+    }
+
+
+    private void selectImage() {
         ImagePicker.with(this)
-                .crop()	    			//Crop image(Optional), Check Customization for more option
-                .compress(1024)			//Final image size will be less than 1 MB(Optional)
-                .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                .crop()                    //Crop image(Optional), Check Customization for more option
+                .compress(1024)            //Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080)    //Final image resolution will be less than 1080 x 1080(Optional)
                 .start();
     }
 
@@ -61,7 +125,7 @@ public class AddRestaurantActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == Activity.RESULT_OK) {
             //Image Uri will not be null for RESULT_OK
-            Uri uri = data.getData();
+            uri = data.getData();
 
             // Use Uri object instead of File to avoid storage permissions
             picRestaurant.setImageURI(uri);
@@ -69,19 +133,26 @@ public class AddRestaurantActivity extends AppCompatActivity {
     }
 
 
-    private boolean validateData(){
-if(restaurantName.getText().toString().isEmpty()){
-    Toast.makeText(AddRestaurantActivity.this,"Please enter a restaurant name! ",Toast.LENGTH_LONG).show();
-    return false;
-}
-if(streetName.getText().toString().isEmpty()){
-    Toast.makeText(AddRestaurantActivity.this, "Please enter a street name! ", Toast.LENGTH_LONG).show();
-    return false;
+    private boolean validateData() {
+        if (restaurantName.getText().toString().isEmpty()) {
+            Toast.makeText(AddRestaurantActivity.this, "Please enter a restaurant name! ", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (streetName.getText().toString().isEmpty()) {
+            Toast.makeText(AddRestaurantActivity.this, "Please enter a street name! ", Toast.LENGTH_LONG).show();
+            return false;
 
-}
-if (postCode.getText().toString().isEmpty()){
-    Toast.makeText(AddRestaurantActivity.this, "Enter a postcode! ", Toast.LENGTH_LONG).show();
+        }
+        if (postCode.getText().toString().isEmpty()) {
+            Toast.makeText(AddRestaurantActivity.this, "Enter a postcode! ", Toast.LENGTH_LONG).show();
+            return false;
+        }
 
-}
+        if(uri==null){
+            Toast.makeText(AddRestaurantActivity.this, "No photo selected ", Toast.LENGTH_LONG).show();
+            return false;
+
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/assignment/HomeActivity.java
+++ app/src/main/java/com/example/assignment/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.assignment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,8 +8,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.assignment.model.RestaurantData;
 import com.example.assignment.model.UserData;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
     Button buttonToHome;
@@ -32,6 +42,21 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+        getData();
+
+    }
+
+    private void getData(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("restaurant")
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    List<RestaurantData> restaurantDataList = task.getResult().toObjects(RestaurantData.class);
+                }
+            }
+        });
 
     }
 
